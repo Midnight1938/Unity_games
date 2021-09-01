33153449530e720d5189c55449055a48e7d6965a
Skshm <eminate894567@gmail.com>
2021-09-01 14:22:40 +0530

    Snek

---
+++ Sn3k/Assets/Scripts/Snake.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    private Vector2 direction = Vector2.right;
+
+    private List<Transform> _segments = new List<Transform>();
+
+    public Transform segmentPrefab;
+    public int initialSize=2;
+
+    private void Start() {ResetState();}
+
+   private void Update()
+    {
+        //* Only allow turning up or down while moving in the x-axis
+        if (this.direction.x != 0f)
+        {
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
+                this.direction = Vector2.up;
+            } else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
+                this.direction = Vector2.down;
+            }
+        }
+        //* Only allow turning left or right while moving in the y-axis
+        else if (this.direction.y != 0f)
+        {
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
+                this.direction = Vector2.right;
+            } else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
+                this.direction = Vector2.left;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        for (int i = _segments.Count-1; i > 0; i--)
+        {
+            _segments[i].position = _segments[i-1].position;
+        }
+
+        this.transform.position =
+            new Vector3(Mathf.Round(this.transform.position.x) + direction.x,
+                Mathf.Round(this.transform.position.y) + direction.y,
+                0.0f);
+    }
+
+    private void Grow()
+    {
+        Transform segment = Instantiate(this.segmentPrefab);
+        segment.position = _segments[_segments.Count - 1].position;
+
+        _segments.Add (segment);
+    }
+
+   public void ResetState()
+    {
+        this.direction = Vector2.right;
+        this.transform.position = Vector3.zero;
+
+        // Start at 1 to skip destroying the head
+        for (int i = 1; i < _segments.Count; i++) {
+            Destroy(_segments[i].gameObject);
+        }
+        _segments.Clear();
+        _segments.Add(this.transform);
+
+        for (int i = 1; i < this.initialSize; i++)
+        {
+            _segments.Add(Instantiate(this.segmentPrefab));
+        }
+
+
+        this.transform.position = Vector3.zero;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Food")
+        {
+            Grow();
+        } else if (other.tag == "Obstacle"){ResetState();}
+    }
+}
